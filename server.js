05a708a294445276433be006f153374a88d148aa
sanchet2 <nsancheti1993@gmail.com>
2015-06-17 16:57:38 -0700

    added elasticsearch

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ var express = require('express'),
 	async = require('async'),
 	moment = require('moment'),
 	uuid = require('uuid'),
-	_ = require('underscore')
+	_ = require('underscore'),
+	elasticsearch = require('elasticsearch')
 
 var app = express()
 app.use(bodyParser.json())
 var jsonParser = bodyParser.json()
 var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:27017/';
+var client = new elasticsearch.Client({
+	host: 'localhost:9200',
+	log: 'trace'
+});
+
+
 
 app.get('/', function (req, res) {
 	res.send("yo mama so fat");
@@ -54,10 +61,10 @@ app.post('/user', function (req, res) {
 	console.log(req.body);
 	async.waterfall([
 			function (callback) {
-				getCurrentUser(req.body.username,function(err,doc,db,collection){
-			    console.log("done");
-					callback(err,doc,db,collection)
-			  })
+				getCurrentUser(req.body.username, function (err, doc, db, collection) {
+					console.log("done");
+					callback(err, doc, db, collection)
+				})
 			},
 			function (doc, db, collection, callback) {
 				if(_.isEmpty(doc)) {
@@ -70,7 +77,7 @@ app.post('/user', function (req, res) {
 
 				} else {
 					//add logic for timestamp
-					if(moment().unix() - doc[0].timestamp > 60 ) {
+					if(moment().unix() - doc[0].timestamp > 60) {
 						req.body.timestamp = moment().unix()
 						req.body.session = uuid.v1();
 						collection.insert(req.body, function (err, result) {
@@ -84,47 +91,86 @@ app.post('/user', function (req, res) {
 			}
 		],
 		function (err, result) {
+			console.log("\n\n")
+
+			console.log(result);
 			if(err) {
 				console.log(err);
 				res.status(403).send("duplicate")
 			} else {
+				client.index({
+					index: 'user',
+					type: 'document',
+					id: result._id.toString(),
+					body: {
+						username: result.username,
+						timestamp: result.timestamp,
+					}
+				}, function (error, response) {
+					console.log(error);
+					console.log(response);
+				});
 				res.send(result)
 			}
 		});
-
 })
 
-app.post("/user/:user",function (req, res){
-	var username=req.params.user
-	var session=req.body.session
-	async.waterfall([
-		function (callback) {
-		getCurrentUser(username,function(err,doc,db,collection){
-			console.log("done");
-			callback(err,doc,db,collection)
-		})
-	},function(doc,db,collection,callback){
-		if(_.isEmpty(doc)==false){
-			if(doc[0].session==session && moment().unix()-doc[0].timestamp<60){
-				res.status(200).send({"status":"continue"})
-				db.close()
-			}
-			else {
-				res.status(403).send({"done":"failed"})
-				db.close()
+app.get("/user/query/:user", function (req, res) {
+	var name = req.params.user;
+	console.log(name)
+	client.search({
+		index: 'user',
+		type: 'document',
+		analyzeWildcard: true,
+		body: {
+			query: {
+				match: {
+					username: name
+				}
 			}
 		}
+	}).then(function (resp) {
+		var hits = resp.hits.hits;
+		res.send(hits);
+	}, function (err) {
+		console.trace(err.message);
+	});
 
-	}
-	])
 })
 
 
 
 
 
+app.post("/user/:user", function (req, res) {
+	var username = req.params.user
+	var session = req.body.session
+	async.waterfall([
+		function (callback) {
+			getCurrentUser(username, function (err, doc, db, collection) {
+				console.log("done");
+				callback(err, doc, db, collection)
+			})
+		},
+		function (doc, db, collection, callback) {
+			if(_.isEmpty(doc) == false) {
+				if(doc[0].session == session && moment().unix() - doc[0].timestamp < 60) {
+					res.status(200).send({
+						"status": "continue"
+					})
+					db.close()
+				} else {
+					res.status(403).send({
+						"done": "failed"
+					})
+					db.close()
+				}
+			}
 
-
+		}
+	])
+})
+2
 
 var server = app.listen(3000, function () {
 
